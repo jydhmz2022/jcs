4b70118b6508598c8553494753a72cbb141f0bdf
jydhmz2022 <jiyid.hamza@gmail.com>
2024-03-28 20:42:16 +0100

    create app jcs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import Accueil from "./components/Accueil";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import {store} from "./store";
+import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
+import LanguageProvider from "./LanguageProvider";
 
 function App() {
+  // const { i18n } = useTranslation();
+  // const currentLanguage = useSelector(
+  //   (state) => state.language.currentLanguage
+  // );
+  // const dispatch = useDispatch()
+
+  // useEffect(() => {
+  //   i18n.changeLanguage(currentLanguage);
+  // }, [currentLanguage, i18n]);
+
+  // const changeLanguage = lng => {
+  //   dispatch({ type: "language/setLanguage", payload: lng }); 
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="main-content">
+          <Sidebar />
+          <div className="content-container">
+            <Routes>
+              <Route exact path="/" element={<Accueil />} />
+              <Route exact path="/services" element={<Services />} />
+              <Route exact path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+    <LanguageProvider/>
+    </Provider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../src/components/styles/appGlobal.css';
+import LanguageSelector from './languageSelector/LanguageSelecror';
+
+function Header() {
+  return (
+    <div className="header">
+      
+  <a href="/"><img src={`${process.env.PUBLIC_URL}/logo.png`}  className='logo' alt='logo'/></a>
+    <div><LanguageSelector/></div>
+    
+    </div>
+  );
+} 
+
+export default Header;
